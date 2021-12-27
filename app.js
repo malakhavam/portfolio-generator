ae9668f2ce9c446f977f82cb8e8f12f6bac894cf
Maryia <malakhava@yahoo.com>
2021-12-27 01:31:44 -0500

    created html

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const profileDataArgs = process.argv.slice(2);
 // Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
+//const printProfileData = profileDataArr => {
     // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
+  //  for (let i = 0; i < profileDataArr.length; i += 1) {
+    //  console.log(profileDataArr[i]);
+    //}
   
-    console.log('================');
+    //console.log('================');
   
     // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
+    // profileDataArr.forEach(profileItem => console.log(profileItem));
+  // };
 
-  printProfileData(profileDataArgs);
+  // printProfileData(profileDataArgs);
+
+  const fs = require('fs');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+const generatePage = require('./src/page-template.js');
