4d8561b70c5f9da1c56b06863885085f74d9fb06
Maryia <malakhava@yahoo.com>
2022-01-09 21:32:24 -0500

    prompts

--- app.js
+++ app.js
@@ -1,32 +1,22 @@
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-//const printProfileData = profileDataArr => {
-    // This...
-  //  for (let i = 0; i < profileDataArr.length; i += 1) {
-    //  console.log(profileDataArr[i]);
-    //}
+const inquirer = require('inquirer');
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+      }
+    ]);
+  };
   
-    //console.log('================');
+  promptUser().then(answers => console.log(answers));
+// const fs = require('fs');
+//const generatePage = require('./src/page-template');
   
-    // Is the same as this...
-    // profileDataArr.forEach(profileItem => console.log(profileItem));
-  // };
-
-  // printProfileData(profileDataArgs);
-
-const fs = require('fs');
-
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2);
-console.log(profileDataArgs);
-
-const [name, github] = profileDataArgs;
-console.log(name, github);
-
-const pageHTML = generatePage(name, github);
-
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//  const pageHTML = generatePage(name, github);
+  
+ // fs.writeFile('./index.html', pageHTML, err => {
+ //   if (err) throw err;
+  
+ //   console.log('Portfolio complete! Check out index.html to see the output!');
+//  });
