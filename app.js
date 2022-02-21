93148981d09875ac41bf46dd2299ce6442bd9b85
Maryia <malakhava@yahoo.com>
2022-02-20 22:36:59 -0500

    first portfolio

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ Add a New Project
         type: 'checkbox',
         name: 'languages',
         message: 'What did you this project with? (Check all that apply)',
-        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        choices: ['JavaScript', 'HTML', 'Handlebars', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node', 'MySQL']
       },
       {
         type: 'input',
